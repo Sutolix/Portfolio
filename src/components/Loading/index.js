9e949fe9ea0b99b6d102c8eaa749bf7ea4abb377
Sutolix <tg884ght@gmail.com>
2020-05-10 12:31:34 -0300

    adição dos projetos

--- src/components/Loading/index.js
+++ src/components/Loading/index.js
@@ -1,28 +1,39 @@
 import React from 'react'
 import styled, { keyframes } from 'styled-components'
 
-export default function Loading ({proficiency}) {
 	const slide = keyframes`
   	0% {
      	width: 0;
    	}
    	100% {
-     	width: ${(proficiency*3)}px;
+     	width: 100%;
    	}
 	 `
-
 	const LoadingBar = styled.div`
   width: 300px;
   height: 4px;
   background: #ccc;
+  position: relative;
     &::before{
     content: '';
     position: absolute;
     height: 4px;
+    max-width: ${props => props.proficiency*3}px;
     background: #037fff;
 	 	animation: 1s ${slide} ease-out;
 	 	animation-fill-mode : forwards;
   }
 	`
-	return (<LoadingBar />);
+//props.proficiency*3 porque o width da barra é 300, ou seja, (proficiency * 300)/100. Simplificando,
+//proficiency * 3
+
+export default function Loading ({proficiency}) {
+
+/*
+	As constantes estão fora da função porque não é certo chamar styled components dentro de outro componente(função),
+	estavam retornando avisos sobre
+*/
+
+	//passando a proficiency para o LoadingBar ter acesso a ele.
+	return (<LoadingBar proficiency={proficiency}/>);
 }
--- src/components/Projetos/index.js
+++ src/components/Projetos/index.js
@@ -3,6 +3,14 @@ import './projeto.css'
 
 import Background from '../Background'
 
+import kodlogo from '../../assets/img/kod-logo.png'
+import weather from '../../assets/img/weather-logo.png'
+import repo from '../../assets/img/repo-logo.png'
+import fale from '../../assets/img/fale-logo.png'
+import calcultor from '../../assets/img/calculator-logo.png'
+
+
+
 export default function Projetos () {
 
   return (
@@ -10,6 +18,76 @@ export default function Projetos () {
   		<Background/>
 
 			<div className="content">
+        <div className="title">
+          <span>Projetos</span>
+        </div>
+        <div className="layer">
+          <span>Projetos</span>
+        </div>
+
+        <div className="projects">
+
+        	<div className="project d-flex">
+        		<a href="https://kodfinder.netlify.app" target="_blank" rel="noopener noreferrer">
+        			<img src={kodlogo} alt="kodfinder bird" />
+        		</a>
+        		<div className="projct-details">
+        			<h5>KodFinder</h5>
+        			<p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quas, magni mollitia, aspernatur
+        			consequatur accusamus vero eum facere exercitationem velit suscipit ipsam placeat libero.
+        			Deleniti exercitationem nostrum quasi. Molestiae, vel porro.</p>
+        		</div>
+        	</div>
+
+        	<div className="project d-flex">
+        		<a href="https://weatherappimap.netlify.app/" target="_blank" rel="noopener noreferrer">
+        		<img src={weather} alt="kodfinder bird" />
+        		</a>
+        		<div className="projct-details">
+        			<h5>Weather App</h5>
+        			<p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quas, magni mollitia, aspernatur
+        			consequatur accusamus vero eum facere exercitationem velit suscipit ipsam placeat libero.
+        			Deleniti exercitationem nostrum quasi. Molestiae, vel porro.</p>
+        		</div>
+        	</div>
+
+        	<div className="project d-flex">
+        		<a href="https://reposearch.netlify.app/" target="_blank" rel="noopener noreferrer">
+        			<img src={repo} alt="kodfinder bird" />
+        		</a>
+        		<div className="projct-details">
+        			<h5>RepoSearch</h5>
+        			<p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quas, magni mollitia, aspernatur
+        			consequatur accusamus vero eum facere exercitationem velit suscipit ipsam placeat libero.
+        			Deleniti exercitationem nostrum quasi. Molestiae, vel porro.</p>
+        		</div>
+        	</div>
+
+        	<div className="project d-flex">
+        		<a href="https://planosfalemais.netlify.com/" target="_blank" rel="noopener noreferrer">
+        			<img src={fale} alt="kodfinder bird" />
+        		</a>
+        		<div className="projct-details">
+        			<h5>FaleMais</h5>
+        			<p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quas, magni mollitia, aspernatur
+        			consequatur accusamus vero eum facere exercitationem velit suscipit ipsam placeat libero.
+        			Deleniti exercitationem nostrum quasi. Molestiae, vel porro.</p>
+        		</div>
+        	</div>
+
+        	<div className="project d-flex">
+        		<a href="https://sutolix.github.io/calculator/" target="_blank" rel="noopener noreferrer">
+        			<img src={calcultor} alt="kodfinder bird" />
+        		</a>
+        		<div className="projct-details">
+        			<h5>Calculator</h5>
+        			<p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quas, magni mollitia, aspernatur
+        			consequatur accusamus vero eum facere exercitationem velit suscipit ipsam placeat libero.
+        			Deleniti exercitationem nostrum quasi. Molestiae, vel porro.</p>
+        		</div>
+        	</div>
+
+        </div>
 
 			</div>
 
