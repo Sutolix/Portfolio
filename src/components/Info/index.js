2c0dd99f37b0e0c8a6a7529a19ba6ae340a520a6
Sutolix <tg884ght@gmail.com>
2020-05-12 09:34:35 -0300

    adição de trabalhos e icons nas techs

---
+++ src/components/Info/index.js
@@ -0,0 +1,95 @@
+export const skills = [
+    {
+      name: 'HTML5',
+      proficiency: '90'
+    },
+    {
+      name: 'CSS3',
+      proficiency: '87'
+    },
+    {
+      name: 'ReactJs',
+      proficiency: '70'
+    },
+    {
+      name: 'Bootstrap',
+      proficiency: '67'
+    },
+    {
+      name: 'Git',
+      proficiency: '60'
+    },
+    {
+      name: 'Photoshop',
+      proficiency: '50'
+    }
+  ]
+
+export const courses = [
+    {
+      name: 'CS50',
+      data: '2020 - Presente',
+      local: 'Harvard University (Online)',
+      description: `CS50 é uma introdução à Ciência da Computação abordando linguagens de programação como C
+                    e Python além de estruturas de dados e SQL. O curso é um dos mais populares de Harvard sendo
+                    executado com uma equipe de profissionais em conjunto ao renomado professor David J. Malan.
+                    Tem aulas presenciais mas que são gravadas e disponibilizadas oficialmente em formato de curso
+                    online com todo suporte e certificações.`
+    },
+    {
+      name: 'JavaScript',
+      data: '2020 - Presente',
+      local: 'Udemy (Online)',
+      description: `Curso ministrado pela HCODE e com vários projetos reais como clones do WhatsApp e Dropbox
+                    usando Firebase e o que há de mais moderno no JavaScript. São mais de 200 aulas indo do básico
+                    ao avançado somando cerca de 38 horas de videoaulas. Express, Socket IO, Chart JS, NodeJS são
+                    algumas das tecnologias usadas durante o curso.`
+    },
+    {
+      name: 'ReactJs',
+      data: '2020 - 2020',
+      local: 'Udemy (Online)',
+      description: `Este curso visa ensinar desde o básico ao avançado do ReactJs. Conta com alguns projetos como blog,
+                    videolocadora, buscador de repositório entre outros. É introduzido o uso de classes mas apresenta os
+                    novos recursos de Hooks mostrando sua maior simplicidade. É apresentado ao aluno o Firebase com seu
+                    banco de dados em tempo real e como isso deixa uma aplicação mais dinâmica.`
+    },
+    {
+      name: 'HTML5, CSS3, Bootstrap 4',
+      data: '2019 - 2019',
+      local: 'Udemy (Online)',
+      description: `Curso completo de desenvolvimento web com criação de um site funcional e responsivo com loja e carrinho
+                    de compras. São mais de 75 aulas computando cerca de 13 horas de videoaulas. Angular, PHP com Slim
+                    Framework e MySQL também estão presentes.`
+    },
+    {
+      name: 'Técnico em Informática',
+      data: '2017 - 2018',
+      local: 'Ponte Nova-MG',
+      description: `Curso presencial com matérias como Empreendedorismo, Análise e Projeto de Sistemas, Redes, Manutenção de
+                   Computadores, Banco de Dados, Linguagem Orientada a Objetos e Programação da Internet. No fim houveram
+                    trabalhos de conclusão de curso englobando todos os conhecimentos adiquiridos durante o período.`
+    }
+  ]
+
+export const works = [
+    {
+      name: 'Professor de Informática',
+      data: '2019 - Presente',
+      local: 'Autônomo',
+      description: `Ensino de Informática básica com foco em ferramentas de escritório como World e Excel.
+                    Criação e formatação de provas e TCC.`
+    },
+    {
+      name: 'Assistente de Compras Online',
+      data: '2017 - Presente',
+      local: 'Autônomo',
+      description: `Compra de produtos nacionais e importados. O cliente escolhe o que quer, faço pesquisas pelos
+                    menores preços, verifico taxas, converso com o vendedor e efetuo a compra além de acompanhar
+                    todo o trâmite do processo. Caso haja algum problema, como o produto chegar danificado ou ser extraviado,
+                    é minha responsibilidade tomar as ações cabíveis, o cliente não precisa se preocupar com nada nesse quesito.
+                    É um serviço útil pois muitas pessoas não tem conhecimento ou mesmo tempo para realizar essas compras, principalmente
+                    as internacionais que dependem de um pouco mais de atenção em relação a tamanhos, padrões de tomada, versões
+                    de software e taxações.`
+    }
+  ]
--- src/components/Resumo/index.js
+++ src/components/Resumo/index.js
@@ -2,84 +2,18 @@ import React from 'react'
 import './resume.css'
 
 import { MdSchool } from 'react-icons/md'
+import { FaSuitcase } from 'react-icons/fa'
+
+import { AiFillHtml5 } from 'react-icons/ai'
+import { DiCss3, DiReact, DiGit, DiPhotoshop } from 'react-icons/di'
+import { BsBootstrap } from 'react-icons/bs'
+
 import Background from '../Background'
 import Loading from '../Loading'
 
+import {skills, courses, works} from '../Info'
+
 export default function Resumo () {
-
-  const skills = [
-  {
-    name: 'HTML5',
-    proficiency: '90'
-  },
-  {
-    name: 'CSS3',
-    proficiency: '87'
-  },
-  {
-    name: 'ReactJs',
-    proficiency: '70'
-  },
-  {
-    name: 'Bootstrap',
-    proficiency: '67'
-  },
-  {
-    name: 'Git',
-    proficiency: '60'
-  },
-  {
-    name: 'Photoshop',
-    proficiency: '50'
-  },
-  ]
-
-  const courses = [
-  {
-    name: 'CS50',
-    data: '2020 - Presente',
-    local: 'Harvard University (Online)',
-    description: `CS50 é uma introdução à Ciência da Computação abordando linguagens de programação como C
-                  e Python além de estruturas de dados e SQL. O curso é um dos mais populares de Harvard sendo
-                  executado com uma equipe de profissionais em conjunto ao renomado professor David J. Malan.
-                  Tem aulas presenciais mas que são gravadas e disponibilizadas oficialmente em formato de curso
-                  online com todo suporte e certificações.`
-  },
-  {
-    name: 'JavaScript',
-    data: '2020 - Presente',
-    local: 'Udemy (Online)',
-    description: `Curso ministrado pela HCODE e com vários projetos reais como clones do WhatsApp e Dropbox
-                  usando Firebase e o que há de mais moderno no JavaScript. São mais de 200 aulas indo do básico
-                  ao avançado somando cerca de 38 horas de videoaulas. Express, Socket IO, Chart JS, NodeJS são
-                  algumas das tecnologias usadas durante o curso.`
-  },
-  {
-    name: 'ReactJs',
-    data: '2020 - 2020',
-    local: 'Udemy (Online)',
-    description: `Este curso visa ensinar desde o básico ao avançado do ReactJs. Conta com alguns projetos como blog,
-                  videolocadora, buscador de repositório entre outros. É introduzido o uso de classes mas apresenta os
-                  novos recursos de Hooks mostrando sua maior simplicidade. É apresentado ao aluno o Firebase com seu
-                  banco de dados em tempo real e como isso deixa uma aplicação mais dinâmica.`
-  },
-  {
-    name: 'HTML5, CSS3, Bootstrap 4',
-    data: '2019 - 2019',
-    local: 'Udemy (Online)',
-    description: `Curso completo de desenvolvimento web com criação de um site funcional e responsivo com loja e carrinho
-                  de compras. São mais de 75 aulas computando cerca de 13 horas de videoaulas. Angular, PHP com Slim
-                  Framework e MySQL também estão presentes.`
-  },
-  {
-    name: 'Técnico em Informática',
-    data: '2017 - 2018',
-    local: 'Ponte Nova-MG',
-    description: `Curso presencial com matérias como Empreendedorismo, Análise e Projeto de Sistemas, Redes, Manutenção de
-                  Computadores, Banco de Dados, Linguagem Orientada a Objetos e Programação da Internet. No fim houveram
-                  trabalhos de conclusão de curso englobando todos os conhecimentos adiquiridos durante o período.`
-  },
-  ]
 
   return (
   	<div className="page">
@@ -97,7 +31,17 @@ export default function Resumo () {
           <div className="skills">
             {skills.map((skill) => (
               <div className="skill" key={skill.name}>
-                <h5>{skill.name}</h5>
+                <div className="d-flex align-center">
+                  {
+                    (skill.name === 'HTML5') ? (<AiFillHtml5 size={22} color="#fff" />) :
+                    (skill.name === 'CSS3') ? (<DiCss3 size={22} color="#fff" />) :
+                    (skill.name === 'ReactJs') ? (<DiReact size={22} color="#fff" />) :
+                    (skill.name === 'Bootstrap') ? (<BsBootstrap size={22} color="#fff" />) :
+                    (skill.name === 'Git') ? (<DiGit size={22} color="#fff" />) :
+                    (<DiPhotoshop size={22} color="#fff" />)
+                  }
+                  <h5>{skill.name}</h5>
+                </div>
                 <div className="skill-graphic d-flex">
                   <span>{skill.proficiency}%</span>
                   <Loading proficiency={skill.proficiency} />
@@ -115,6 +59,30 @@ export default function Resumo () {
   			<div className="layer">
 					<span>RESUMO</span>
 				</div>
+
+        <div className="subtitle">
+          <h5 className="d-flex"><FaSuitcase size={30} color="#fff"/>Trabalhos</h5>
+        </div>
+
+        <div className="historic">
+
+        {works.map((work) => (
+
+          <div className="steps" key={work.name}>
+            <div className="sumary">
+              <h6>{work.data}</h6>
+            </div>
+            <div className="details">
+              <h5 className="select-color">{work.name}</h5>
+              <h6>{work.local}</h6>
+              <p>{work.description}</p>
+            </div>
+          </div>
+
+        ))}
+
+
+        </div>
 
   			<div className="subtitle">
   				<h5 className="d-flex"><MdSchool size={30} color="#fff"/>Estudos</h5>
